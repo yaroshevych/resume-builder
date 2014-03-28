e566fba371df145de2278e3fb517b33319f30048
Oleg Yaroshevych <oleg@yaroshevy.ch>
2014-03-28 18:28:41 +0200

    Added document.save on backend

--- routes/documents.js
+++ routes/documents.js
@@ -5,20 +5,29 @@ module.exports = function(app) {
     app.get('/api/documents', session.isAuthenticated, function(req, res) {
         mongoose.models.Document.find({}, function(err, records) {
             if (err) {
-                return res.send(400);
-            }
-
-            var documents = [];
-
-            for (var i = 0; i < records.length; i++) {
-                var doc = records[i].toJSON();
-                doc.id = doc._id;
-                delete doc._id;
-                documents.push(doc);
+                return res.json(400, {
+                    message: err + ''
+                });
             }
 
             res.json({
-                documents: documents
+                documents: records
+            });
+        });
+    });
+
+    app.put('/api/documents/:id', session.isAuthenticated, function(req, res) {
+        mongoose.models.Document.findByIdAndUpdate(req.params.id, {
+            $set: req.body.document
+        }, function(err, rec) {
+            if (err) {
+                return res.json(400, {
+                    message: err + ''
+                });
+            }
+
+            res.json({
+                document: rec
             });
         });
     });
