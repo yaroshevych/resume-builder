962b151b48e3308d4b7e1c5f5e47b56e8c3e5289
Oleg Yaroshevych <oleg@yaroshevy.ch>
2014-04-04 20:00:12 +0300

    Filter documents by name

--- client/scripts/controllers/DocumentsController.js
+++ client/scripts/controllers/DocumentsController.js
@@ -1,1 +1,16 @@
-App.DocumentsController = Ember.ArrayController.extend({});
+App.DocumentsController = Ember.ArrayController.extend({
+    nameFilter: '',
+
+    filteredContent: function() {
+        var content = this.get('content'),
+            nameFilter = this.get('nameFilter').toLowerCase();
+
+        if (nameFilter) {
+            return content.filter(function(item) {
+                return (item.get('name') || '').toLowerCase().indexOf(nameFilter) !== -1;
+            });
+        } else {
+            return content;
+        }
+    }.property('content', 'nameFilter')
+});
