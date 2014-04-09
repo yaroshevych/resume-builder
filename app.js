74e5bc1e38b7a3460cbd48671dbb82eea371000d
Oleg Yaroshevych <oleg@yaroshevy.ch>
2014-04-09 16:57:16 +0300

    Don't use cluster in dev environment

--- app.js
+++ app.js
@@ -1,152 +1,162 @@
 'use strict';
 
-var cluster = require('cluster'),
-    numCPUs = require('os').cpus().length;
-
-if (cluster.isMaster) {
-    for (var i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('death', function(worker) {
-        console.log('worker ' + worker.pid + ' died');
-        cluster.fork();
-    });
-} else {
-    var express = require('express'),
-        path = require('path'),
-        app = express(),
-        session = require('./middleware/session'),
-        passport = session.init(app),
-        port = 3000,
-        intel = require('intel'),
-        mongoose = require('mongoose'),
-        initIntel = function() {
-            intel.config({
-                formatters: {
-                    'simple': {
-                        'format': '[%(levelname)s] %(message)s',
-                        'colorize': true
-                    },
-                    'details': {
-                        'format': '[%(date)s] %(name)s.%(levelname)s: %(message)s',
-                        'datefmt': '%Y-%m-%d %H:%M-%S.%L'
-                    }
+var useCluster = process.env.NODE_ENV === 'production',
+    isWorker = true,
+    intel = require('intel'),
+    mongoose = require('mongoose'),
+    initIntel = function() {
+        intel.config({
+            formatters: {
+                'simple': {
+                    'format': '[%(levelname)s] %(message)s',
+                    'colorize': true
                 },
-                handlers: {
-                    'terminal': {
-                        'class': intel.handlers.Console,
-                        'formatter': 'simple',
-                        'level': intel.VERBOSE
-                    },
-                    'logfile': {
-                        'class': intel.handlers.Rotating,
-                        'level': intel.DEBUG,
-                        maxSize: 1000000,
-                        maxFiles: 10,
-                        'file': 'logs/app.log',
-                        'formatter': 'details'
-                    }
-                },
-                loggers: {
-                    'root': {
-                        'handlers': ['logfile', 'terminal'],
-                        'level': 'VERBOSE',
-                        'handleExceptions': true,
-                        'exitOnError': false,
-                        'propagate': false
-                    },
-                    'root.node_modules.express': { // huh what? see below :)
-                        'handlers': ['terminal'],
-                        'level': 'VERBOSE'
-                    }
+                'details': {
+                    'format': '[%(date)s] %(name)s.%(levelname)s: %(message)s',
+                    'datefmt': '%Y-%m-%d %H:%M-%S.%L'
                 }
-            });
-
-            intel.console();
-        },
-        initMongoose = function() {
-            var connectionString = 'mongodb://localhost/resume-builder';
-
-            if (process.env.NODE_ENV !== 'production') {
-                mongoose.set('debug', function(collectionName, method, query, doc, opt) {
-                    intel.debug('%s %s %s %s $s', collectionName, method, JSON.stringify(query), JSON.stringify(doc), opt);
-                });
+            },
+            handlers: {
+                'terminal': {
+                    'class': intel.handlers.Console,
+                    'formatter': 'simple',
+                    'level': intel.VERBOSE
+                },
+                'logfile': {
+                    'class': intel.handlers.Rotating,
+                    'level': intel.DEBUG,
+                    maxSize: 1000000,
+                    maxFiles: 10,
+                    'file': 'logs/app.log',
+                    'formatter': 'details'
+                }
+            },
+            loggers: {
+                'root': {
+                    'handlers': ['logfile', 'terminal'],
+                    'level': 'VERBOSE',
+                    'handleExceptions': true,
+                    'exitOnError': false,
+                    'propagate': false
+                },
+                'root.node_modules.express': { // huh what? see below :)
+                    'handlers': ['terminal'],
+                    'level': 'VERBOSE'
+                }
             }
+        });
 
-            mongoose.connect(connectionString, {
-                db: {
-                    safe: true
-                }
-            }, function(err, res) {
-                if (err) {
-                    console.log('ERROR connecting to: ' + connectionString + '. ' + err);
-                } else {
-                    console.log('Successfully connected to: ' + connectionString);
-                }
+        intel.console();
+    },
+    initExpress = function() {
+        var express = require('express'),
+            path = require('path'),
+            app = express(),
+            session = require('./middleware/session'),
+            passport = session.init(app),
+            port = 3000;
+
+        app.use(express.favicon());
+        app.use(express.compress());
+        app.use(express.json());
+        app.use(express.urlencoded());
+        app.use(express.methodOverride());
+        app.use(express.cookieParser('cfacee66-586e-4cfe-be97-86c42870f064')); //default secret - will have to be changed
+        app.use(express.bodyParser());
+
+        if (process.env.NODE_ENV === 'production') {
+            app.use('/', express['static'](path.join(__dirname, '/dist'), {
+                maxAge: 31557600000 // one year
+            }));
+        } else {
+            app.use('/scripts', express['static'](path.join(__dirname, '/.tmp/scripts'), {
+                maxAge: 0
+            }));
+
+            app.use('/', express['static'](path.join(__dirname, '/client'), {
+                maxAge: 0
+            }));
+
+            intel.info('Adding latency to simulate real-life connection...');
+            app.use(function(req, res, next) {
+                setTimeout(next, 50);
             });
-        };
-
-    initIntel();
-
-    app.use(express.favicon());
-    app.use(express.compress());
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('cfacee66-586e-4cfe-be97-86c42870f064')); //default secret - will have to be changed
-    app.use(express.bodyParser());
-
-    if (process.env.NODE_ENV === 'production') {
-        app.use('/', express['static'](path.join(__dirname, '/dist'), {
-            maxAge: 31557600000 // one year
-        }));
-    } else {
-        app.use('/scripts', express['static'](path.join(__dirname, '/.tmp/scripts'), {
-            maxAge: 0
-        }));
-
-        app.use('/', express['static'](path.join(__dirname, '/client'), {
-            maxAge: 0
-        }));
-
-        intel.info('Adding latency to simulate real-life connection...');
-        app.use(function(req, res, next) {
-            setTimeout(next, 50);
-        });
-    }
-
-    app.use(express.cookieSession({
-        cookie: {
-            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         }
-    })); // keep sessions small, so no need to persist info in DB
 
-    app.use(passport.initialize());
-    app.use(passport.session());
+        app.use(express.cookieSession({
+            cookie: {
+                maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+            }
+        })); // keep sessions small, so no need to persist info in DB
 
-    app.use(function(req, res, next) {
-        intel.debug('+ %s %s', req.method, req.url);
+        app.use(passport.initialize());
+        app.use(passport.session());
 
-        req.on('end', function() {
-            intel.debug('- %s %s', req.method, req.url);
+        app.use(function(req, res, next) {
+            intel.debug('+ %s %s', req.method, req.url);
+
+            req.on('end', function() {
+                intel.debug('- %s %s', req.method, req.url);
+            });
+
+            next();
         });
 
-        next();
-    });
+        app.use(app.router);
 
-    app.use(app.router);
+        require('./routes')(app);
+        require('./models')(app);
 
-    require('./routes')(app);
-    require('./models')(app);
+        app.get('/', function(req, res) {
+            res.redirect('/index.html');
+        });
 
-    app.get('/', function(req, res) {
-        res.redirect('/index.html');
-    });
+        app.listen(process.env.PORT || port);
+        intel.info('Express started on port ' + port + ', environment: ' + process.env.NODE_ENV + ', process: ' + process.pid);
 
-    app.listen(process.env.PORT || port);
-    intel.info('Express started on port ' + port + ', environment: ' + process.env.NODE_ENV +
-        ', process: ' + process.pid + ', worker: ' + cluster.worker.id);
+    },
+    initMongoose = function() {
+        var connectionString = 'mongodb://localhost/resume-builder';
 
+        if (process.env.NODE_ENV !== 'production') {
+            mongoose.set('debug', function(collectionName, method, query, doc, opt) {
+                intel.debug('%s %s %s %s $s', collectionName, method, JSON.stringify(query), JSON.stringify(doc), opt);
+            });
+        }
+
+        mongoose.connect(connectionString, {
+            db: {
+                safe: true
+            }
+        }, function(err, res) {
+            if (err) {
+                console.log('ERROR connecting to: ' + connectionString + '. ' + err);
+            } else {
+                console.log('Successfully connected to: ' + connectionString);
+            }
+        });
+    };
+
+if (useCluster) {
+    var cluster = require('cluster'),
+        numCPUs = require('os').cpus().length;
+
+    if (cluster.isMaster) {
+        for (var i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('death', function(worker) {
+            console.log('Worker ' + worker.pid + ' died');
+            cluster.fork();
+        });
+
+        isWorker = false;
+    }
+}
+
+if (isWorker) {
+    initIntel();
+    initExpress();
     initMongoose();
 }
